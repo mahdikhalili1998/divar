63c20a9a4376f5efdcc9ee5cdfce0ae282854e66
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-02-22 15:30:51 +0330

    install react query

---
+++ src/constant/viteConfige.js
@@ -0,0 +1,11 @@
+export const path = [
+  "src",
+  "services",
+  "styles",
+  "util",
+  "assets",
+  "components",
+  "configs",
+  "pages",
+  "Router",
+];
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const path = [
-  "src",
-  "services",
-  "styles",
-  "util",
-  "assets",
-  "components",
-  "configs",
-  "pages",
-  "Router",
-];
+import { Paths } from "./src/constant/viteConfige";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      ...path.reduce(
+      ...Paths.reduce(
         (acc, cur) => ({
           ...acc,
           [cur]: `/${cur === "src" ? cur : "src/" + cur}`,
