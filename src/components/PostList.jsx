c4be5f337fa30257b248d529ad20338885227fd5
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2024-03-03 14:08:00 +0330

    get post

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -4,8 +4,20 @@ import { getPost } from "../services/user";
 
 function PostList() {
   const { data, isLoading } = useQuery(["get-post"], getPost);
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  console.log(baseUrl);
   console.log(data);
-  return <div>PostList</div>;
+  return (
+    <>
+      {data?.data.posts.map((item) => (
+        <div key={item._id}>
+          <img src={` ${import.meta.env.VITE_BASE_URL}${item.images[0]}`} />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default PostList;
+
+//"http://localhost:3400/upload/1708607970728.jpg"
